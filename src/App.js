d6e0db56e7dc39317544b1ddb6d9de66b4d46a51
Cleberton Francisco <clebertonfgc@gmail.com>
2021-02-27 18:20:32 -0300

    Base do Redux pronta {store/actions/reducers}

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Cep from './components/Cep';
+import { Provider } from 'react-redux';
+import store from './redux/store/index';
 
 function App() {
   return (
     <div className="App">
-     <Cep />
+      <Provider store={ store }>
+        <Cep />
+      </Provider>
     </div>
   );
 }
---
+++ src/redux/reducer/searchCep.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = [];
+
+function searchCep(state = INITIAL_STATE, action) {
+    switch(action.type){
+        case '':
+            return state;
+        default:
+          return state;
+    }
+}
+
+export default searchCep;
